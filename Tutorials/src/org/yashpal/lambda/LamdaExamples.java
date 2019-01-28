38cb96b762dd1ded990b2bdfca94fe5b991d547e
Yashpal Meena <yashpal@avanti.in>
2019-01-28 16:00:06 +0530

    Hello World :)

---
+++ Tutorials/src/org/yashpal/lambda/LamdaExamples.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright - Yashpal Meena <yaxhpal@gmail.com>
+ */
+package org.yashpal.lambda;
+
+/**
+ * This class is used to test and learn about Java Functional programming i.e. 
+ * Lambda Expressions
+ * 
+ * @author Yashpal Meena <yaxhpal@gmail.com>
+ *
+ */
+public class LamdaExamples {
+  
+	public static void main(String[] args) {
+		System.out.println("Hello World! :)");
+	}
+}
